814b20fa79603055d24d7fa34777855daff406ca
Dylan Taft <dylbronjames@gmail.com>
2020-01-19 22:45:59 -0700

    added testing for async api calls

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
@@ -6,7 +8,11 @@ const functions = {
     const user = { firstName: 'Dylan' }
     user['lastName'] = 'Taft';
     return user;
-  }
-}
+  },
+  //axios call to json placeholder
+  fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
+};
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -43,3 +43,23 @@ test('Admin should be in usernames', () => {
   expect(usernames).toContain('admin');
 });
 
+//working with async data
+//promise
+test('User fetched name should be Leanne Graham promise handle', () => {
+  //must include or test will always pass
+  expect.assertions(1);
+  return functions.fetchUser()
+    .then(data => {
+      expect(data.name).toEqual('Leanne Graham');
+    })
+});
+
+//async await
+test('User fetched name should be Leanne Graham async await', async () => {
+  //must include or test will always pass
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
+
+
